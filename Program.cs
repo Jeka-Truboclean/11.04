fb01a7275955d4d0718a16148118421c76fc8605
Eugen Karaman <karamanjekakol@gmail.com>
2024-04-15 17:17:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _11._04
+{
+    public static class StringExtensions
+    {
+        public static string CapitalizeSentences(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            string[] lines = input.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].CapitalizeFirstLetter();
+            }
+            return string.Join(Environment.NewLine, lines);
+        }
+
+        public static string CapitalizeFirstLetter(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            return char.ToUpper(input[0]) + input.Substring(1);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "input.txt";
+            string text = File.ReadAllText(filePath);
+            string result = text.CapitalizeSentences();
+            Console.WriteLine(result);
+        }
+    }
+}
